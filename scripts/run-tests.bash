7df645391583720d5eb062950de6afd61315d7d4
Richard Fall <rfall@notwitsend.com>
2021-07-02 20:05:13 -0500

    Testing more

--- scripts/run-tests.bash
+++ scripts/run-tests.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-# if any command inside script returns error, exit and return that error 
+  
+# if any command inside script returns error, exit and return that error
 set -e
 
 # magic line to ensure that we're always inside the root of our application,
@@ -8,14 +8,23 @@ set -e
 # thanks to it we can just enter `./scripts/run-tests.bash`
 cd "${0%/*}/.."
 
-# let's fake failing test for now 
+# let's fake failing test for now
 echo "Running tests"
-echo "............................" 
-echo "Succeeded!" && exit 0
+
+files=`git diff --name-only /Users/rfall/DEsktop/hook-testing`
+for file in $files; do
+  echo $file
+
+  if grep -q -l -e '|passwd' $file; then
+    echo "found pattern in" $file
+  fi
+done
+
+echo "Done!" && exit 0
 
 # example of commands for different languages
 # eslint .         # JS code quality check
 # npm test         # JS unit tests
 # flake8 .         # python code quality check
-# nosetests        # python nose 
-# just put your usual test command here 
+# nosetests        # python nose
+# just put your usual test command here
